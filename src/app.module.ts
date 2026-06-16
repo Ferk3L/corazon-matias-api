0140e52573d9e0978cab7f24b37b66a05eaef72a
Ferk3L <gabritoel3@gmail.com>
2026-06-16 01:54:55 -0600

    fix: auth.module.ts correcto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { VerificationService } from './verification.service';
-import { MailModule } from '../mail/mail.module';
-import { FirebaseModule } from '../firebase/firebase.module';
+import { ConfigModule } from '@nestjs/config';
+import { FirebaseModule } from './firebase/firebase.module';
+import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
+import { CashbackModule } from './cashback/cashback.module';
+import { AuthModule } from './auth/auth.module';
+import { MailModule } from './mail/mail.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [FirebaseModule, MailModule],
-  controllers: [AuthController],
-  providers: [VerificationService],
-  exports: [VerificationService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    FirebaseModule,
+    ProductsModule,
+    OrdersModule,
+    CashbackModule,
+    MailModule,
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AuthModule {}
+export class AppModule {}
